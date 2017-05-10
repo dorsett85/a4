22d2355fbf4511ea4d5a260b0b3d568c9eee166d
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2017-05-09 23:26:48 -0400

    Updated object notation

--- resources/views/pages/favorites.blade.php
+++ resources/views/pages/favorites.blade.php
@@ -19,20 +19,20 @@
 
     @if(!$favorites->isEmpty())
         @foreach($favorites as $value)
-            <div id="{{ $value['ticker'] }}" class="favCompany">
-                <h3>{{ $value['company_name'] }}</h3>
+            <div id="{{ $value->ticker }}" class="favCompany">
+                <h3>{{ $value->company_name }}</h3>
                 <div>
-                    <b>Symbol</b>: {{ $value['ticker'] }}<br>
-                    <b>Exchange</b>: {{ $value['stock_exchange'] }}<br>
-                    <b>Company URL</b>: <a href="{{$value['company_url'] }}" class="companyLink"
-                                           target="_blank">{{ $value['company_url'] }}</a><br>
-                    <b>State Headquarters</b>: {{ $value['hq_state'] }} <br>
-                    <b>Sector</b>: {{ $value['sector'] }}<br>
-                    <b>Industry Category</b>: {{ $value['industry_category'] }} <br>
-                    <b>Industry Group</b>: {{ $value['industry_group'] }} <br>
+                    <b>Symbol</b>: {{ $value->ticker }}<br>
+                    <b>Exchange</b>: {{ $value->stock_exchange }}<br>
+                    <b>Company URL</b>: <a href="{{$value->company_url }}" class="companyLink"
+                                           target="_blank">{{ $value->company_url }}</a><br>
+                    <b>State Headquarters</b>: {{ $value->hq_state }} <br>
+                    <b>Sector</b>: {{ $value->sector }}<br>
+                    <b>Industry Category</b>: {{ $value->industry_category }} <br>
+                    <b>Industry Group</b>: {{ $value->industry_group }} <br>
                     <b>Tags</b>:
                     @foreach($allFavoritesTags as $name => $tags)
-                        @if($name == $value['company_name'])
+                        @if($name == $value->company_name)
                             {{ $tags }}
                         @endif
                     @endforeach
@@ -40,7 +40,7 @@
                 <form class="inlineBtn" action="/data" method="post">
                     {{ csrf_field() }}
                     <button class="btn-xs btn-success favoriteInfo">Description</button>
-                    <input type="hidden" name="ticker" value="{{ $value['ticker'] }}">
+                    <input type="hidden" name="ticker" value="{{ $value->ticker }}">
                     <input type="hidden" name="data" value="data">
                     <button type="submit" class="btn-xs btn-primary">
                         Get Data
@@ -48,12 +48,12 @@
                 </form>
                 <form class="inlineBtn" action="/favorites" method="post">
                     {{ csrf_field() }}
-                    <button type="submit" class="btn-xs btn-danger" name="remove" value="{{ $value['id'] }}">
+                    <button type="submit" class="btn-xs btn-danger" name="remove" value="{{ $value->id }}">
                         Remove From Favorites
                     </button>
                 </form>
                 <div class="shortDescription">
-                    {{ $value['short_description'] }}
+                    {{ $value->short_description }}
                 </div>
             </div>
             <hr>
